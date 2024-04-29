eede0f5dcd7babb9ef03506be0c0a72d14294350
Julien ssm <89165787+IjulienI@users.noreply.github.com>
2024-04-29 16:55:46 +0200

    [wip] basic movements

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movements")]
+
+    [SerializeField]private int acceleration = 150;
+    [SerializeField]private int maxSpeed = 500;
+    [SerializeField]private int rotationAcceleration = 50;
+    [SerializeField]private float dragForce;
+
+    private float speed;
+    private float rotation;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //applique les mouvements et les rotation
+        ApplyMovement();
+        ApplyDrag();
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            AddTransform();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            RemoveTransform();
+        }
+    }
+
+
+    void AddTransform()
+    {
+        if(speed < maxSpeed) speed += acceleration * Time.deltaTime;
+    }
+
+    void RemoveTransform()
+    {
+        if(speed > -maxSpeed) speed -= acceleration * Time.deltaTime;
+    }
+
+    void AddRotation()
+    {
+        rotation += rotationAcceleration * Time.deltaTime;
+    }
+
+    void RemoveRotation()
+    {
+        rotation -= rotationAcceleration * Time.deltaTime;
+    }
+
+    void ApplyMovement()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, rotation));
+    }
+
+    void ApplyDrag()
+    {
+        transform.position -= transform.right * (dragForce * (1 * speed)) * Time.deltaTime;
     }
 }
