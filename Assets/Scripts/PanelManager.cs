ac2887846fcc2d9bfb71ef8dc00a54339c6ee7a1
Sacha Lignon <131878820+Dish3s@users.noreply.github.com>
2024-04-30 15:47:55 +0200

    MUSIC

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Properties;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelManager : MonoBehaviour
 {
-    public void QuestButton()
-    {
 
+    private void ClearAll()
+    {
+        for(int i = 0; i < transform.childCount -1; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
 
-
+    public void EnablePanel(int index)
+    {
+        ClearAll();
+        Transform quest = transform.GetChild(index);
+        quest.gameObject.SetActive(true);
+        //Color color = quest.GetComponent<Image>().color;
+        //quest.GetComponent<Image>().color = new Color(color.r,color.g,color.b,130);
+    }
 }
